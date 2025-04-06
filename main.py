95d8e8cd7d7993dcfd7ca359e2bb928eeeceab24
Yamir Alejandro Poldo Silva <126733391+Yamires@users.noreply.github.com>
2025-04-05 22:54:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from config import engine, SessionLocal
 from models import Base, CoupeDuMonde
 
-# Crée les tables si elles n'existent pas encore
+
 Base.metadata.create_all(bind=engine)
 
-# Test simple
 db = SessionLocal()
 coupes = db.query(CoupeDuMonde).all()
 print(coupes)
