17af4eac16f6319f2e880821dd825babdbc0b1af
Yamires <126733391+Yamires@users.noreply.github.com>
2025-04-07 01:04:36 -0400

    corrections

--- create_tables.py
+++ create_tables.py
@@ -1,7 +1,7 @@
 from config import engine, SessionLocal
 from models import Base, CoupeDuMonde
 
-
+Base.metadata.drop_all(bind=engine)  
 Base.metadata.create_all(bind=engine)
 
 db = SessionLocal()
