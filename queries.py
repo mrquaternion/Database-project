9be731534a5299e6b9d9d5ade62fc5781c90e922
Hazem Ben Amor <69153224+Hazoumaba@users.noreply.github.com>
2025-04-07 13:34:51 -0400

    queries + streamlit

--- queries.py
+++ queries.py
@@ -1,6 +1,29 @@
 ## TODO 
+# les équipes avec le plus grand nombres de cartons rouges dans l'histoire du coupe du monde entre 1986-2022
+Query_1 = """SELECT equipe.nom_pays, COUNT(joueur.id) AS nombre_cartons_rouges
+FROM joueur
+JOIN equipe ON joueur.equipe_id = equipe.id
+WHERE joueur.carton_rouge >= 1
+GROUP BY equipe.nom_pays
+ORDER BY nombre_cartons_rouges DESC ;"""
 
-Query_1 = """"""
-Query_2 = """"""
-Query_3 = """"""
-Query_4 = """"""
+# les arbitres qui ont arbitré le plus de parties
+Query_2 = """SELECT arbitre.nom, arbitre.prenom, COUNT(arbitre_dans.partie_id) AS nombre_parties
+FROM arbitre
+JOIN arbitre_dans ON arbitre.id = arbitre_dans.arbitre_id
+GROUP BY arbitre.nom, arbitre.prenom
+ORDER BY nombre_parties DESC;"""
+
+# les stades qui ont accueilli plus de 5 parties
+Query_3 = """SELECT stade.nom, stade.ville, COUNT(partie.id) AS nombre_parties
+FROM stade
+JOIN partie ON stade.id = partie.stade_id
+GROUP BY stade.nom, stade.ville
+HAVING COUNT(partie.id) >= 5;"""
+
+# les parties avec un score total supérieur à 6
+Query_4 = """SELECT partie.date, partie.phase, equipe_recevante.nom_pays AS equipe_recevante, partie.score_recevante, equipe_visiteuse.nom_pays AS equipe_visiteuse, partie.score_visiteuse
+FROM partie
+JOIN equipe AS equipe_recevante ON partie.equipe_recevante_id = equipe_recevante.id
+JOIN equipe AS equipe_visiteuse ON partie.equipe_visiteuse_id = equipe_visiteuse.id
+WHERE (partie.score_recevante + partie.score_visiteuse) > 6;"""
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,53 @@
+import os
 import streamlit as st
 from sqlalchemy import  create_engine, text
 import pandas as pd
 from queries import Query_1, Query_2, Query_3, Query_4
 
-DB_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DB_URL, echo=True)
+# CSS
 
-## TODO Faire l'interface ici 
+st.markdown(
+    """
+    <style>
+    .stButton>button {
+        font-size: 20px;
+        padding: 15px 30px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# DB_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5433/postgres"
+engine = create_engine(DATABASE_URL, echo=True)
+
+## TODO Faire l'interface ici
+def execute_query(query):
+    with engine.connect() as connection:
+        result = connection.execute(text(query))
+        df = pd.DataFrame(result.fetchall(), columns=result.keys())
+    return df
+
+# Streamlit app
+st.title("La coupe du monde des nations du football")
+
+# Query 1
+if st.button("Équipes avec le plus grand nombre de cartons rouges"):
+    df = execute_query(Query_1)
+    st.write(df)
+
+# Query 2
+if st.button("Arbitres qui ont arbitré le plus de parties"):
+    df = execute_query(Query_2)
+    st.write(df)
+
+# Query 3
+if st.button("Stades qui ont accueilli plus de 5 parties"):
+    df = execute_query(Query_3)
+    st.write(df)
+
+# Query 4
+if st.button("Parties avec un score total supérieur à 6"):
+    df = execute_query(Query_4)
+    st.write(df)
